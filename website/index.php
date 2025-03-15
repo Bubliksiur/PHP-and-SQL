35d20adc6a6ec5d25c35f9ea2268863900bd0e30
Bubliks <149240768+Bubliksiur@users.noreply.github.com>
2025-03-15 22:05:13 +0100

    funkcja php

---
+++ website/index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        body{
+            background-color: #232323;
+            color: #fff;
+            font-family: sans-serif;
+            font-weight: bold;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        p{
+            display: block;
+            margin: 20px;
+            animation: jump 1s infinite; /* Użyj animacji */
+        }
+/* Definicja animacji */
+@keyframes jump {
+    0%, 100% {
+        transform: translateY(0); /* Początkowa i końcowa pozycja */
+    }
+    50% {
+        transform: translateY(-20px); /* Podskok w górę */
+    }
+}
+
+/* Ustawienie opóźnienia dla każdego p */
+p:nth-child(1) {
+    animation-delay: 0s;
+}
+p:nth-child(2) {
+    animation-delay: 0.5s;
+}
+p:nth-child(3) {
+    animation-delay: 1s;
+}
+p:nth-child(4) {
+    animation-delay: 1.5s;
+}
+    </style>
+</head>
+<body>
+    <?php  
+        function sayHello($user){
+            echo 'Hello ' . $user;
+        }
+    ?>
+
+        <p><?php sayHello("Jakub")?></p>
+        <p><?php sayHello("Adrian")?></p>
+        <p><?php sayHello("Piotr")?></p>
+        <p><?php sayHello("Justyna")?></p>
+</body>
+</html>
